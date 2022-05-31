ac6af98ff42d8747c0c667b559d77d99e892209d
Abukes <susolek@gmail.com>
2022-05-31 11:37:45 +0200

    Algorytmika

---
+++ MotifEnumeration.py
@@ -0,0 +1,91 @@
+"""
+Jakub Susoł 274300
+Zadanie 1 zaliczenie
+Algorytmika
+
+Algorytm MotifEnumeration
+(strona 71 Bioinformatics Algorithms - An Active Learning Approach vol. I)
+"""
+import os
+import sys
+import json
+import time
+
+
+def readSequences():
+    if not os.path.isfile('./compiled_sequences.fasta'):
+        if not os.path.isdir('./sequences'):
+            print("There seems to not be a 'sequences' folder in current directory."
+                  "This directory is essential for the program to work. It will be now created."
+                  "Please move all sequence FASTA files to be worked on into this directory and run the program again")
+            os.mkdir('./sequences')
+            print('The program will stop in 10 seconds.')
+            time.sleep(10)
+            if sys.platform == 'win32':
+                os.system('pause')
+            elif sys.platform == 'linux' or sys.platform == 'linux2' or sys.platform == 'darwin':
+                os.system('read -n1 -r -p "Press any key to continue..."')
+        else:
+            with open("compiled_sequences.fasta", 'w') as f:
+                for sequence in os.listdir('./sequences'):
+                    with open(f'./sequences/{sequence}', 'rt') as s:
+                        readseq = ''
+                        for line in s.readlines()[1:76]:
+                            readseq += line.strip('\n')
+                        readseq += '\n'
+                        f.writelines(readseq)
+            with open('./compiled_sequences.fasta', 'rt') as f:
+                DNA = f.readlines()
+    else:
+        with open('./compiled_sequences.fasta', 'rt') as f:
+            DNA = f.readlines()
+    return DNA
+
+
+# K-Mer is a (k,d)-motif if it appears in every line of DNA with at most d mismatches
+def motifEnumeration(dna: list, k: int, d: int):
+    patterns = dict()  # new patterns set
+    refSeq = dna[0]
+    patternExistsIn = dict()
+    for compDna in dna[1::]:
+        # for each k-mer pattern in dna
+        for i in range(len(refSeq) - k):
+            pattern = refSeq[i:i + k]
+            patternExistsIn[pattern] = [(0, pattern)]
+            print(f'Comparing {i + 1}/{len(refSeq) - k} in {dna.index(compDna)} of {len(dna) - 1}')
+            # for each pattern' differing from pattern by at most d mismatches
+            for j in range(len(compDna) - k):
+                patternPrime = compDna[j:j + k]
+                # check mismatches
+                mismatches = 0
+                for a, b in zip(pattern, patternPrime):
+                    if a == b:
+                        pass
+                    else:
+                        mismatches += 1
+                # add index of DNA string the pattern' exists in and the pattern
+                if mismatches <= d:
+                    patternExistsIn[pattern].append((dna.index(compDna), patternPrime))
+    # if pattern' exists in all lines of DNA
+    for key, value in patternExistsIn.items():
+        if len(value) == len(dna):
+            patternlist = []
+            for item in value:
+                patternlist.append(item[1])
+            # add pattern to patterns
+            patterns[value[0][1]] = patternlist
+    return patterns
+
+
+def createJSON(DNA, k, d):
+    motifdict = motifEnumeration(DNA, k, d)
+    if not os.path.isdir('./motifs'):
+        os.mkdir('./motifs')
+    with open(f'./motifs/({k},{d})-motifs.json', 'w') as m:
+        json.dump(motifdict, m)
+    createdFile = f'({k},{d})-motifs.json'
+    return motifdict, createdFile
+
+
+if __name__ == '__main__':
+    createJSON(readSequences(), 12, 3)
---
+++ Motifs_Definitions.py
@@ -0,0 +1,129 @@
+"""
+Jakub Susoł 274300
+Zadanie 1 zaliczenie
+Algorytmika
+
+Algorytmy obliczające Score, Count, Profile i Consensus dla motywów uzyskanych od MotifEnumeration.
+(Bioinformatics Algorithms - An Active Learning Approach vol. I)
+"""
+import os
+import json
+import re
+import time
+
+
+def loadDataOrCreate():
+    motifsDirectoryList = os.listdir('./motifs')
+    # print(motifsDirectoryList)
+    motifslist = []
+    if motifsDirectoryList:
+        for file in motifsDirectoryList:
+            match = re.match(r'(.+)\.json', file)
+            if match is not None:
+                motifslist.append(match.string)
+            else:
+                pass
+        fileloadchoice = True
+        while fileloadchoice:
+            indices = [i for i in range(len(motifslist))]
+            for i, file in zip(indices, motifslist):
+                print(f'[{i}] - {file}')
+            try:
+                chosenfile = int(input('\nPlease choose a file to load data from:\n>>> '))
+                with open(f'./motifs/{motifslist[chosenfile]}') as f:
+                    motifsdict = json.load(f)
+                fileloadchoice = False
+                return motifsdict, motifslist[chosenfile]
+            except ValueError:
+                print(
+                    'The number you chose is either not included on available list or Your input is not a '
+                    'number.\nPlease input a number from the list of possible inputs.')
+            except IndexError:
+                print(
+                    'The number you chose is either not included on available list or Your input is not a '
+                    'number.\nPlease input a number from the list of possible inputs.\n')
+    else:
+        from MotifEnumeration import readSequences, createJSON
+        while True:
+            try:
+                k = int(input('Input k integer (how long a mer is) for motif enumeration\nK = '))
+                d = int(input('Input d integer (how many mismatches there are at maximum) for motif enumeration\nD = '))
+                break
+            except ValueError:
+                print("The number you input is not a valid integer, please try again.\n")
+        print("The program will now create a motifs file and read it. Starting in 5 seconds")
+        time.sleep(5)
+        motifsdict, createdFile = createJSON(readSequences(), k, d)
+        return motifsdict, createdFile
+    pass
+
+
+def defineMotifs(motifsDict):
+    opDict = dict()
+    for key, item in motifsDict.items():
+        opDict[key] = {}
+        opDict[key]["Motifs"] = item
+        opDict[key]["Score"] = 0
+        opDict[key]["Count"] = {"A": [0 for _ in range(len(item[0]))],
+                                "C": [0 for _ in range(len(item[0]))],
+                                "G": [0 for _ in range(len(item[0]))],
+                                "T": [0 for _ in range(len(item[0]))]}
+        opDict[key]["Profile"] = {"A": [],
+                                  "C": [],
+                                  "G": [],
+                                  "T": []}
+        opDict[key]["Consensus"] = ''
+        listedSeqs = [list(motSeq) for motSeq in motifsDict[key]]
+
+        # Count
+
+        for nucleotide in range(len(listedSeqs[0])):
+            for sequence in range(len(listedSeqs)):
+                if listedSeqs[sequence][nucleotide] == 'T':
+                    opDict[key]["Count"]["T"][nucleotide] += 1
+                elif listedSeqs[sequence][nucleotide] == 'G':
+                    opDict[key]["Count"]["G"][nucleotide] += 1
+                elif listedSeqs[sequence][nucleotide] == 'C':
+                    opDict[key]["Count"]["C"][nucleotide] += 1
+                elif listedSeqs[sequence][nucleotide] == 'A':
+                    opDict[key]["Count"]["A"][nucleotide] += 1
+
+        # Profile
+
+        for opKey, opItem in opDict[key]["Count"].items():
+            for value in opItem:
+                percent = value / len(item)
+                opDict[key]["Profile"][opKey].append(percent)
+
+        # Consensus
+
+        pKeys = [pKey for pKey in opDict[key]["Profile"].keys()]
+        pItems = [pItem for pItem in opDict[key]["Profile"].values()]
+        for nucIndex in range(len(pItems[0])):
+            percents = []
+            for nucleotide in range(len(pKeys)):
+                percents.append(pItems[nucleotide][nucIndex])
+            opDict[key]["Consensus"] += pKeys[percents.index(max(percents))]
+
+        # Score
+
+        listedConsensus = list(opDict[key]["Consensus"])
+        for sequence in listedSeqs:
+            for cNuc, sNuc in zip(listedConsensus, sequence):
+                if cNuc != sNuc:
+                    opDict[key]["Score"] += 1
+    return opDict
+
+
+def makeComparisonJSON(motifsDict, file):
+    opDict = defineMotifs(motifsDict)
+    if not os.path.isdir('./motifs/definitions'):
+        os.mkdir('./motifs/definitions')
+    with open(f'./motifs/definitions/({file})_definitions.json', 'w') as d:
+        json.dump(opDict, d)
+    return opDict
+
+
+if __name__ == '__main__':
+    motifsDict, file = loadDataOrCreate()
+    makeComparisonJSON(motifsDict, file)
